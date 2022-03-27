34c2c298fe8cc04f8cbf7dba722eeca80181642d
John Tucker <jtucker@spotify.com>
2022-03-27 05:07:33 -0400

    sorting

--- src/App.js
+++ src/App.js
@@ -29,55 +29,100 @@ const sortCommunityAsc = (a, b) => a.community - b.community;
 
 const sortCommunityDec = (a, b) => b.community - a.community;
 
+const sortEcologicalAsc = (a, b) => a.ecological - b.ecological;
+
+const sortEcologicalDec = (a, b) => b.ecological - a.ecological;
+
+const sortEconomicAsc = (a, b) => a.economic - b.economic;
+
+const sortEconomicDec = (a, b) => b.economic - a.economic;
+
 function App() {
   const [indicators, setIndicators] = useState(window.fpiIndicators.sort(sortNameAsc));
+  const [column, setColumn] = useState('name');
+  const [ascending, setAscending] = useState(true);
 
-  const handleSortNameAscClick = useCallback(
+  const handleSortNameClick = useCallback(
+    () => {
+      const newIndicators = [...indicators];
+      let newAscending = true;
+      if (!ascending || column !== 'name' ) {
+        newIndicators.sort(sortNameAsc);
+        newAscending = true;
+      } else {
+        newIndicators.sort(sortNameDec);
+        newAscending = false;
+      }
+      setColumn('name');
+      setAscending(newAscending);
+      setIndicators(newIndicators);
+    }, [indicators, column, ascending],
+  );
+
+  const handleSortCommunityClick = useCallback(
     () => {
       const newIndicators = [...indicators];
       newIndicators.sort(sortNameAsc);
+      let newAscending = true;
+      if (!ascending || column !== 'community' ) {
+        newIndicators.sort(sortCommunityAsc);
+        newAscending = true;
+      } else {
+        newIndicators.sort(sortCommunityDec);
+        newAscending = false;
+      }
+      setColumn('community');
+      setAscending(newAscending);
       setIndicators(newIndicators);
-    }, [indicators],
+    }, [indicators, column, ascending],
   );
 
-  const handleSortNameDecClick = useCallback(
+  const handleSortEcologicalClick = useCallback(
     () => {
       const newIndicators = [...indicators];
-      newIndicators.sort(sortNameDec);
+      newIndicators.sort(sortNameAsc);
+      let newAscending = true;
+      if (!ascending || column !== 'ecological' ) {
+        newIndicators.sort(sortEcologicalAsc);
+        newAscending = true;
+      } else {
+        newIndicators.sort(sortEcologicalDec);
+        newAscending = false;
+      }
+      setColumn('ecological');
+      setAscending(newAscending);
       setIndicators(newIndicators);
-    }, [indicators],
+    }, [indicators, column, ascending],
   );
 
-  const handleSortCommunityAscClick = useCallback(
+  const handleSortEconomicClick = useCallback(
     () => {
       const newIndicators = [...indicators];
-      newIndicators.sort(sortCommunityAsc);
+      newIndicators.sort(sortNameAsc);
+      let newAscending = true;
+      if (!ascending || column !== 'economic' ) {
+        newIndicators.sort(sortEconomicAsc);
+        newAscending = true;
+      } else {
+        newIndicators.sort(sortEconomicDec);
+        newAscending = false;
+      }
+      setColumn('economic');
+      setAscending(newAscending);
       setIndicators(newIndicators);
-    }, [indicators],
-  );
-
-  const handleSortCommunityDecClick = useCallback(
-    () => {
-      const newIndicators = [...indicators];
-      newIndicators.sort(sortCommunityDec);
-      setIndicators(newIndicators);
-    }, [indicators],
+    }, [indicators, column, ascending],
   );
 
   return (
     <>
     <div>
-      <button onClick={handleSortNameAscClick}>Sort Name Asc</button>
-      <button onClick={handleSortCommunityAscClick}>Sort Community Asc</button>
-      <button>Sort Ecological Asc</button>
-      <button>Sort Economic Asc</button>
+      <button onClick={handleSortNameClick}>Sort Name</button>
+      <button onClick={handleSortCommunityClick}>Sort Community</button>
+      <button onClick={handleSortEcologicalClick}>Sort Ecological</button>
+      <button onClick={handleSortEconomicClick}>Sort Economic</button>
     </div>
-    <div>
-      <button onClick={handleSortNameDecClick}>Sort Name Dec</button>
-      <button onClick={handleSortCommunityDecClick}>Sort Community Dec</button>
-      <button>Sort Ecological Dec</button>
-      <button>Sort Economic Dec</button>
-    </div>
+    <div>{ascending ? 'ASCENDING' : 'DESCENDING'}</div>
+    <div>{column}</div>
     {indicators.map((indicator) => (
       <Indicator
         community={indicator.community}
